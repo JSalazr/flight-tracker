6d7d544b11dd9731ba7ee41c46f1e86ccee2b965
JSalazr <jonathansalazar96@gmail.com>
2020-01-28 10:20:17 -0600

    Further configurations.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './src/App';
 
-ReactDOM.render(
-  React.createElement("div", null, "Hello World"), // <div>Hello World</div>
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById('root'));
