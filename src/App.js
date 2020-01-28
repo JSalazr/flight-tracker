bc5e9560a90b22719a00fefeb40e476c6e2fabff
JSalazr <jonathansalazar96@gmail.com>
2020-01-28 15:54:07 -0600

    Added sass to the project. Added form for search.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import LandingPage from './components/landingPage';
+import './styles/styles.scss';
 
 class App extends React.Component {
   render() {
 
     return (
-		<div>
-			Hello World
-		</div>	
+			<div className="container">
+				<LandingPage className="landingPage" />	
+			</div>
     );
   }
 }
